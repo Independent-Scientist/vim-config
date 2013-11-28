2eaa6fab115b2f20e3e2d38be6533e3881c57db7
Leonardo da Silva <independent.scientist@gmail.com>
2013-11-28 01:50:59 -0300

    go

--- vim/bundle/bootstrap.sh
+++ vim/bundle/bootstrap.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/davidhalter/jedi-vim.git
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/Lokaltog/powerline.git
-git clone git://github.com/tpope/vim-fugitive.git
+#!/bin/bash
+
+git clone https://github.com/Valloric/YouCompleteMe.git
+git clone https://github.com/scrooloose/syntastic
