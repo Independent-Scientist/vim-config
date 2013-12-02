8a3dccda05dfec57d845fc8851bf97ccc97785f5
Leonardo da Silva <independent.scientist@gmail.com>
2013-12-02 07:04:40 -0300

    more plugins

--- vim/bundle/bootstrap.sh
+++ vim/bundle/bootstrap.sh
@@ -12,3 +12,4 @@ git clone https://github.com/croaker/mustang-vim
 git clone https://github.com/vim-scripts/pep8
 git clone https://github.com/vim-scripts/mru.vim
 git clone git://github.com/tpope/vim-fugitive.git
+git clone https://github.com/majutsushi/tagbar
